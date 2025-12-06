df354d26c89b4ec740155e5f92ef05ca0e2470fb
Jeyster <gaurat.mathieu@gmail.com>
2025-12-06 21:52:54 +0100

    Add request logging filter

--- src/main/java/com/jeyster/gestionnaire_collection_tcg/controller/interfaces/GameController.java
+++ src/main/java/com/jeyster/gestionnaire_collection_tcg/controller/interfaces/GameController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@RequestMapping("/game")
+@RequestMapping("/v1/games")
 public interface GameController {
 
     @GetMapping
