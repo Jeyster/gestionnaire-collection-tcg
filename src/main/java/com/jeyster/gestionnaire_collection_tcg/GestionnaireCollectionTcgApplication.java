8c333ce495568b7107bae19c469bfd988e173d63
Jeyster <gaurat.mathieu@gmail.com>
2025-12-06 17:44:47 +0100

    Initialisation projet springboot

---
+++ src/main/java/com/jeyster/gestionnaire_collection_tcg/GestionnaireCollectionTcgApplication.java
@@ -0,0 +1,13 @@
+package com.jeyster.gestionnaire_collection_tcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionnaireCollectionTcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionnaireCollectionTcgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeyster/gestionnaire_collection_tcg/GestionnaireCollectionTcgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeyster.gestionnaire_collection_tcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionnaireCollectionTcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
